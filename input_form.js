30f13e4273759f0902f54e89e4fbba74adcd3e4d
Pratyush-002 <130606501+Pratyush-002@users.noreply.github.com>
2023-04-13 02:13:46 +0530

    Personal_Portfolio
    Hi, I am Pratyush Anand. To Know More About Me.Please Deep Dive Into This  Portfolio.

---
+++ input_form.js
@@ -0,0 +1,25 @@
+const loginForm = document.getElementById("form-input");
+
+
+loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    let username = document.getElementById("username");
+    let comment = document.getElementById("comment");
+    let email = document.getElementById("email")
+  
+    if (username.value == "" || comment.value == "" || email.value =="") {
+      alert("Ensure you input a value in all the fields!");
+    } else {
+      alert("This form has been successfully submitted!");
+      console.log(
+        `This form has a username of ${username.value} and comment of ${comment.value}`
+      );
+  
+      username.value = "";
+      comment.value = "";
+      email.value = "";
+    }
+  });
+
+  
